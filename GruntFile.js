2c081f0a8be0c1ff3d271e3b41ca9c4fa5da5a10
karasov <s@xyz.in.ua>
2015-08-27 17:49:17 +0300

    Init commit

---
+++ GruntFile.js
@@ -0,0 +1,123 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),				
+		
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				eqnull: true,				
+				browser: true,
+				globals: {
+					jQuery: true,
+					$: true,
+					console: true
+				}
+			},
+			'<%= pkg.name %>': {
+				src: [ 'src/js/**/*.js' ]
+			},
+		},
+		
+		concat: {
+			options: {
+				stripBanners: true,
+				banner: '/* <%= pkg.name %> - v<%= pkg.version %> */\n'
+			},
+			dist: {
+				src: [ 
+					'src/js/*',
+					'src/js/*/*'
+				],
+				dest: 'www-root/js/build.js'
+			}
+		},
+		
+		uglify: {
+			build: {
+				src: 'www-root/js/build.js',
+				dest: 'www-root/js/build.min.js'
+			}
+		},
+		
+		cssmin: {
+			with_banner: {
+				options: {
+					banner: '/* CSS */'
+				},
+				
+				files: {
+					'www-root/css/style.min.css' : [ 'src/css/*.css' ]
+				}
+			}
+		},
+
+		less: {
+  			development: {
+    			options: {
+      				paths: ["assets/css"]
+    			},
+				files: {
+      				"src/css/compiled-style.css": "src/less/style.less"
+    			}
+			}
+		},
+
+		watch: {
+			files: [
+				'src/*/*',
+				'src/*/*/*',
+				'www-root/*'
+			],
+			tasks: ['jshint', 'concat', 'uglify', 'less', 'cssmin'],
+			options: { livereload: true }
+		},
+	
+		connect: {
+    		server: {
+      			options: {
+        			port: 9000,
+        			base: 'www-root',
+      			}
+    		}
+		},
+
+		lineremover: {
+			html: {
+		    	files: [
+		        	{
+		            	expand: true,
+		            	cwd: 'www-root/',
+		            	src: ['**/*.html'],
+		            	dest: 'www-root/',
+		            	ext: '.html'
+		          	}
+		        ]
+			}
+		},
+		
+	
+	});
+	
+	// Загрузка задач
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-line-remover');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	
+	// Задачи по умолчанию
+	grunt.registerTask("default", ['jshint', 'concat', 'uglify', 'less', 'cssmin', 'lineremover', 'watch']);
+	
+	// Запустить простой веб - сервер
+	grunt.registerTask("server", ['connect::server', 'watch']);	
+}
---
+++ src/js/dev/livereload.js
@@ -0,0 +1,7 @@
+/* jshint ignore:start */
+(function(){
+    document.write('<script src="http://' + location.host.split(':')[0] + ':35729/livereload.js"></' + 'script>');
+})(
+    
+);
+/* jshint ignore:end */
